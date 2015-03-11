8edc65242038840c5679bfb4af01df4012aef8a4
Easwaran Ramamurthy <eramamur@andrew.cmu.edu>
2015-03-10 23:30:18 -0400

    adding Aniket's Question 1 code

---
+++ Project02/Project2Question1.m
@@ -0,0 +1,28 @@
+% Implementation of a Gaussian Filter.
+
+%Reading Image.
+I = imread('image1.tiff');
+
+%Asking user for input of sigma.
+sigma = input('Please Enter the value of sigma:');
+
+%Assuming size of gaussain matrix as 15 by 15.
+hsize = [15 15];
+
+%Creating a two-dimensional Gaussian filter h.
+h = fspecial('gaussian', hsize , sigma);
+
+%Convolving the 2D image with the gaussian filter.
+I2 = conv2(I,h);
+
+% Displaying the original image, then after getting sigma input, displaying
+% filtered image.
+figure, imshow(I)
+figure, imshow(I2/256)
+
+%Comments below
+% Gaussian filter is essentially a low-pass filter. Thus, it removes noise
+% but also removes detail.
+% Large sigma means a wider Gaussain filter means greater smoothening
+% Thus as sigma goes from 1 to 7 , we see a greater blur of the image as
+% greater amount of detail is removed.
---
+++ Project02/Project2Question2.m
@@ -0,0 +1,28 @@
+% Implementation of a Gaussian Filter.
+
+%Reading Image.
+I = imread('image1.tiff');
+
+%Asking user for input of sigma.
+sigma = input('Please Enter the value of sigma:');
+
+%Assuming size of gaussain matrix as 15 by 15.
+hsize = [15 15];
+
+%Creating a two-dimensional Gaussian filter h.
+h = fspecial('gaussian', hsize , sigma);
+
+%hx is gradient along x-axis direction and hy is gradient along y-axis
+%direction
+[hx,hy] = gradient(h);
+
+%Convolving the 2D image with the gaussian filter in the x direction.
+I2 = conv2(I,hx);
+
+%Convolving the 2D image with the gaussian filter in the y direction.
+I3 = conv2(I,hy);
+
+%Displaying original image, followed by derivative images.
+figure, imshow(I)
+figure, imshow(I2)
+figure, imshow(I3);
