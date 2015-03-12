69bb4d18abb54bbc79a03e07313cbed7a4112513
sidd312 <sgurdasa@andrew.cmu.edu>
2015-03-12 17:49:04 -0400

    adding code for q2.1

---
+++ Project02/Question2/q2background.m
@@ -0,0 +1,23 @@
+function [  ] = q2background( input_folder_name, output_folder_name )
+imagefiles = dir([input_folder_name,'/*.tif']);
+num_images = length(imagefiles);
+%read the series of images.
+for k = 1:num_images
+    %extract the file name
+    currentfilename = imagefiles(k).name;
+    %read the current file
+    A = imread([input_folder_name,'/',currentfilename]);
+    %get the initial position for cropping the image
+    if k == 1
+        figure, imshow(A,[]); h = imrect; position = wait(h);
+    end
+    %crop the region
+    I = imcrop(A,position);
+    %scale the image
+    normalizedImage = uint8(255*mat2gray(I));
+    %output image path with the image name
+    S = sprintf([output_folder_name,'/', sprintf('background%0.3d.tif',k)]);
+    %write the image to the output
+    imwrite(normalizedImage,S);
+end
+end
