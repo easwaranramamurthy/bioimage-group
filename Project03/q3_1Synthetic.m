c27e22556be0cfd1c11884060af34b5a358d5ce3
PRopp42 <focusSwordsman@gmail.com>
2015-04-04 21:00:26 -0400

    Finished Synthetic function

---
+++ Project03/q3_1Synthetic.m
@@ -0,0 +1,23 @@
+function [ outputImage ] = q3_1Synthetic( I, lambda, numAp, pixelSize, avgBG, stdBG )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+sigma = 0.61*lambda/(pixelSize*numAp);
+
+kernelSize = 6*round(sigma);
+
+hsize = [kernelSize kernelSize];
+
+%Creating a two-dimensional Gaussian filter h.
+h = fspecial('gaussian', hsize , sigma);
+
+%Convolving the 2D image with the gaussian filter.
+outputImage = conv2(double(I),double(h), 'same');
+maxVal = 2535 / max(max(outputImage))
+outputImage = outputImage * maxVal;
+background = randn(size(I));
+background = (background*stdBG)+avgBG;
+outputImage = outputImage + background;
+outputImage = uint16(outputImage);
+
+end
+
