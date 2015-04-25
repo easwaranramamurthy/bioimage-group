463c10784b2e1355511cd1d89803df3db7f133c2
Easwaran Ramamurthy <eramamur@andrew.cmu.edu>
2015-04-25 16:32:54 -0400

    code for tuning SOT parameters added

--- Project04/performSOT.m
+++ Project04/performSOT.m
@@ -1,6 +1,4 @@
-function [ segmentedImage ] = performSegmentationUsingITK( imageFile, method )
-
-I = double(imread(imageFile));
+function [ segmentedImage ] = performSOT( I, numHistograms )
 
 imgSize = size(I);
 
@@ -9,10 +7,9 @@ I2 = zeros(imgSize(1), imgSize(2),2);
 I2(1:imgSize(1),1:imgSize(2),1) = I(1:imgSize(1),1:imgSize(2));
 I2(1:imgSize(1),1:imgSize(2),2) = I(1:imgSize(1),1:imgSize(2));
 
+segmentedImage = matitk('SOT', numHistograms, double(I2));
 
-segmentedImage = matitk(method, [max(I2(:))], double(I2));
-
-
+segmentedImage = squeeze(segmentedImage(:,:,1));
 
 end
 
