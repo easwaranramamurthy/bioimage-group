166b3d18633dbedddbcbd40de90aae9100ed6b2f
sidd312 <sgurdasa@andrew.cmu.edu>
2015-05-06 21:31:43 -0400

    added output files for segment algorithm

--- Project04/C4/segment/parameter_tunesegment.py
+++ Project04/C4/segment/parameter_tunesegment.py
@@ -4,9 +4,9 @@ import sys
 
 def main():
 	input_file = sys.argv[1]
-	for K in [500,600,700,800,900,1000]:
-		for minVal in [50,100,150,200]:
-			for sigma in [0.5,1,1.5,2,2.5,3]:
+	for K in [500,1000]:
+		for minVal in [100,150]:
+			for sigma in [1,1.5,2,2.5]:
 				output = "output_sigma_" + str(sigma) + "_K" + str(K) + "_min" + str(minVal) + ".tif"
 				command = "./segment "+str(sigma) +" "+ str(K) +" " + str(minVal) + " " + input_file + " " + output
 				#print command
